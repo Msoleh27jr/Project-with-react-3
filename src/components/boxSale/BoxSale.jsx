b37642f0efa276df944c38fbba554ff8ffb25612
Soleh.jr <mahmadievsoleh.jr@gmail.com>
2025-04-17 14:18:41 +0500

    using with map

--- src/components/boxSale/BoxSale.jsx
+++ src/components/boxSale/BoxSale.jsx
@@ -2,21 +2,51 @@ import React from 'react'
 import './boxSale.css'
 
 const BoxSale = ({saleImg , imgHero , likeImg , autoDivasBtn , Buttons , Buttons2}) => {
+  let boxs = [
+    {
+      like : likeImg ,
+      sale : saleImg ,
+      hero : imgHero ,
+      auto : autoDivasBtn ,
+      btn1 : Buttons ,
+      btn2 : Buttons2
+    }
+    // ,{
+    //   like : likeImg ,
+    //   sale : saleImg ,
+    //   hero : imgHero ,
+    //   auto : autoDivasBtn ,
+    //   btn1 : Buttons ,
+    //   btn2 : Buttons2
+    // },
+    // {
+    //   like : likeImg ,
+    //   sale : saleImg ,
+    //   hero : imgHero ,
+    //   auto : autoDivasBtn ,
+    //   btn1 : Buttons ,
+    //   btn2 : Buttons2
+    // }
+  ]
   return (
     <div>
-        <div className="box">
-          <div style={{widows : '100%', display : 'flex', alignItems : 'center', justifyContent : 'space-between'}}>
-            <img src={saleImg} alt="" />
-            <img src={likeImg} alt="" />
+        {boxs.map((e , i)=>{
+          return (
+            <div className="box" key={i}>
+          <div style={{width : '100%', display : 'flex', alignItems : 'center', justifyContent : 'space-between'}}>
+            <img src={e.sale} alt="" />
+            <img src={e.like} alt="" />
           </div>
-          <img style={{width : '100%', marginTop : '15px'}} src={imgHero} alt="" />
-          <h2><img src={autoDivasBtn} alt="" />₦600,000</h2>
+          <img style={{width : '100%', marginTop : '15px'}} src={e.hero} alt="" />
+          <h2><img src={e.auto} alt="" />₦600,000</h2>
           <h3>Nokia Essential Wireless Headphones 120CM BLACK</h3>
           <div style={{display : 'flex', alignItems : 'center', width : '100% ', justifyContent : 'space-between'}}>
-          {Buttons2}
-          {Buttons}
+          {e.btn1}
+          {e.btn2}
           </div>
         </div>
+          )
+        })}
     </div>
   )
 }
